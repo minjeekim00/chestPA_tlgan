ff3d2b9dffd76b085c0e24ae9355d8acb3e8cca9
minjee <minjeekim00@gmail.com>
2019-03-27 17:13:53 +0900

    save predictions

--- 1_discover_feature_axis.py
+++ 1_discover_feature_axis.py
@@ -11,17 +11,17 @@ import h5py
 import shutil
 from tqdm import tqdm
 
-path = './src/model/chestPA_classifier'
-sys.path.append(path)
-from src.model.chestPA_classifier.model import classifier
-from src.model.chestPA_classifier import test
+sys.path.append('./src/model/chestPA_classifier')
+from model import classifier
+from model.classifier import classes
+import chestPA_test
+
 
 # sample curation
-
 def select_samples(dict_zx): # sigmoid 0.9 이상값만 선별 
     z = dict_zx['z']
     x = dict_zx['x']
-    list_pred = test(x)
+    list_pred = chestPA_test.test(x)
     list_z = []
     list_y = []
     for i, pred in enumerate(list_pred):
@@ -37,15 +37,19 @@ def select_samples(dict_zx): # sigmoid 0.9 이상값만 선별
     if len(list_z) != 0:
         list_z = np.vstack(list_z)
         list_y = np.asarray(list_y, dtype=np.int8).T
-        print("{} out of {} are selected".format(len(list_z), len(z)))
-        return list_z, list_y
+        #print("{} out of {} are selected".format(len(list_z), len(z)))
+        return list_z, list_y, list_pred
     else:
-        print("nothing was selected")
-        return None, None
+        #print("nothing was selected")
+        return None, None, list_pred
     
     
-# path to model generated results
+# path to model generated results# path to model generated results
+
 path_gan_sample = './asset_results/pggan_x_ray_integrated_norm_sample_pkl/'
+path_pred_sample = os.path.join(path_gan_sample, "predictions")
+if not os.path.exists(path_pred_sample):
+    os.mkdir(path_pred_sample)
 list_pkl = sorted(glob.glob(path_gan_sample+'*.pkl'))
 
 # initialization
@@ -54,12 +58,14 @@ arr_y = np.empty([1], dtype=np.int8)
 
 for file_pkl in tqdm(list_pkl[:]):
     with open(file_pkl, 'rb') as f:
-        file_pkl_num = os.path.splitext(os.path.basename(file_pkl))[0][-6:].strip('0')
         dict_zx = pickle.load(f)
-        z, y = select_samples(dict_zx)
+        z, y, preds = select_samples(dict_zx)
         if z is not None and y is not None:
             arr_z = np.concatenate((arr_z, z), 0)
             arr_y = np.concatenate((arr_y, y), 0)
+        basename_pkl = os.path.splitext(os.path.basename(file_pkl))[0]
+        path_preds = os.path.join(path_pred_sample, basename_pkl+'.npy')
+        np.save(path_preds, preds) # saving predictions
         
 ## save as h5file
 path_sample_jpg = './asset_results/pggan_x_ray_integrated_norm_sample_jpg/'
