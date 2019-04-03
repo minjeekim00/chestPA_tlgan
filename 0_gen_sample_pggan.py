9c02c4deeeec383f693e7decaa2cc2fc3e2e583a
minjee <minjeekim00@gmail.com>
2019-04-03 18:19:24 +0900

    generate image from random seed

--- 0_gen_sample_pggan.py
+++ 0_gen_sample_pggan.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import pickle
+import random
+import time
 import numpy as np
 import tensorflow as tf
 import PIL.Image
@@ -12,8 +14,8 @@ path_model = './asset_model/x-ray_integrated_20190218_network-snapshot-014000.pk
 sys.path.append(path_pg_gan_code)
 
 # path to model generated results
-path_gen_sample = './asset_results/pggan_x_ray_integrated_norm_sample_pkl/'
-path_fake_images = './data/x-ray_integrated_norm/'
+path_gen_sample = './asset_results/sample_pkl/'
+path_fake_images = './data/images/'
 if not os.path.exists(path_gen_sample):
     os.mkdir(path_gen_sample)
 if not os.path.exists(path_fake_images):
@@ -23,6 +25,9 @@ if not os.path.exists(path_fake_images):
 n_batch = 1000
 batch_size = 30
 
+def get_randnum(time):
+    return int((time - int(time))* 1000000.0)
+
 with tf.Session() as sess:
     try:
         with open(path_model, 'rb') as file:
@@ -30,34 +35,28 @@ with tf.Session() as sess:
     except FileNotFoundError:
         print('before running the code, download pre-trained model to project_root/asset_model/')
         raise
-
-    # Generate latent vectors.
-    # latents = np.random.RandomState(1000).randn(1000, *Gs.input_shapes[0][1:]) # 1000 random latents
-    # latents = latents[[477, 56, 83, 887, 583, 391, 86, 340, 341, 415]] # hand-picked top-10
-    latents = np.random.RandomState(1000).randn(n_batch * batch_size, *Gs.input_shapes[0][1:])
-    
-    for i_batch in tqdm(range(n_batch)):
-        i_sample = i_batch * batch_size
-
-        p_latents = latents[i_batch:i_batch+batch_size]
-        labels = np.zeros([p_latents.shape[0]] + Gs.input_shapes[1][1:])
-
-        # Run the generator to produce a set of images.
-        images = Gs.run(p_latents, labels)
-        images = np.clip(np.rint((images + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8)  # [-1,1] => [0,255]
-        images = images.transpose(0, 2, 3, 1)  # NCHW => NHWC
-        images = images.reshape(p_latents.shape[0],1024,1024)
-            
-            
-        # Save images as PNG.
-        for idx in range(images.shape[0]):
-            path = path_fake_images+'img%06d.png'%int(idx+i_batch)
-            if os.path.exists(path):
-                continue
-            PIL.Image.fromarray(images[idx], 'L').save(path)
-
-        path_pickle = os.path.join(path_gen_sample, 'pggan_x_ray_integrated_norm_{:0>6d}.pkl'.format(i_sample))
-        with open(path_pickle, 'wb') as f:
-            pickle.dump({'z': p_latents, 'x': images}, f)
-            
-        del p_latents, labels, images
+        
+    latents = []
+    images = []
+    for idx in tqdm(range(n_batch * batch_size)[:]):
+        
+        latent = np.random.RandomState(get_randnum(time.time())).randn(1, *Gs.input_shapes[0][1:])
+        label = np.zeros([1] + Gs.input_shapes[1][1:])
+        image = Gs.run(latent, label)
+        image = np.clip(np.rint((image + 1.0) / 2.0 * 255.0), 0.0, 255.0).astype(np.uint8)# [-1,1] => [0,255]
+        image = image.reshape(1024, 1024)
+        path = path_fake_images+'img%06d.png'%int(idx)
+        PIL.Image.fromarray(image, 'L').save(path)
+        
+        latents.append(latent)
+        images.append(image)
+        
+        if idx !=0 and idx % 30 == 0:
+            path_pickle = os.path.join(path_gen_sample, 'pggan_x_ray_integrated_norm_{:0>6d}.pkl'.format(idx))
+            latents = np.asarray(latents, dtype=np.float32)
+            images = np.asarray(images, dtype=np.uint8)
+            with open(path_pickle, 'wb') as f:
+                pickle.dump({'z': latents, 'x': images}, f)
+            # re-initialize
+            latents = []
+            images = []
